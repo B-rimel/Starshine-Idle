f7932ce723ecb9df11ee484984f6896aa56aca84
B-rimel <luca.afev78@gmail.com>
2024-11-27 16:14:02 +0100

    Cleaning unused code

--- Starshine Idle/src/components/StarSidebar/StarSidebar.vue
+++ Starshine Idle/src/components/StarSidebar/StarSidebar.vue
@@ -8,6 +8,7 @@
       <p>{{ star.starName }}</p>
       <p>Owned : {{ star.owned }}</p>
       <p>{{ star.description }}</p>
+      <p>Next upgrade : {{ star.cost }} Stardust</p>
       <p>Generates {{ star.stardustGeneration }} Stardust</p>
       <button v-on:click="event => buyStar(star, event)">Level up!</button>
     </div>
@@ -23,23 +24,13 @@ import { useCurrencyStore } from '../../stores/currency'
 
 const currencyStore = useCurrencyStore()
 
-function buyStar(star, event) {
+function buyStar(star: object) {
   console.log(star)
-  const boundingBox = event.target.getBoundingClientRect()
-  console.log(boundingBox.left, boundingBox.top)
-  console.log(boundingBox.right, boundingBox.bottom)
-
-  const isMouseOverButton =
-    event.clientX >= boundingBox.left &&
-    event.clientX <= boundingBox.right &&
-    event.clientY >= boundingBox.top &&
-    event.clientY <= boundingBox.bottom
-  console.log(isMouseOverButton)
 
   const previsionalCost = star.cost * Math.pow(1.03, star.owned)
   if (previsionalCost < currencyStore.stardustCount) {
     console.log('La currency est suffisante', currencyStore.stardustCount)
-    star.cost = previsionalCost
+    star.cost = Math.floor(previsionalCost)
     star.owned += 1
     currencyStore.stardustCount -= previsionalCost
     console.log(star)
