c51f0e04127ef5540612e35ce4acc04cf015924b
B-rimel <luca.afev78@gmail.com>
2025-02-13 12:42:09 +0100

    Removing unused getter

--- Starshine Idle/src/stores/currency.ts
+++ Starshine Idle/src/stores/currency.ts
@@ -7,10 +7,6 @@ export const useCurrencyStore = defineStore('currency', {
     stardustGeneration: 0
   }),
 
-  getters:  () => {
-
-  },
-
   actions: {
     resetStardust() {
       this.stardustCount = 0
